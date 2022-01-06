885624a415af78dcda8dade3aeaf1cad7da5338a
vivek-2000 <parthikpatel2000@gmail.com>
2022-01-07 00:03:12 +0530

    day 5

---
+++ Milestone 1 - Goldman Sachs/MinimumSizeSubarraySum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        i=0
+        minn=float('inf')
+        n=len(nums)
+        summ=0
+        j=0
+        while j<n:
+            summ+=nums[j]
+            while i<n and summ>=target:
+                minn=min(minn,j-i+1)
+                summ-=nums[i]
+                i+=1
+            j+=1
+        if minn==float('inf'):
+            return 0
+            
+        return minn
+                
+            
+        
