8c9e658d79b1edeb2d7b79c4d9b39dcebd8fb59e
vivek-2000 <parthikpatel2000@gmail.com>
2022-01-09 02:08:28 +0530

    day 8

---
+++ Milestone 2 - Amazon/Maximum of all subarrays of size k.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+from heapq import heappop,heappush
+class Solution:
+    
+    #Function to find maximum of each subarray of size k.
+    def max_of_subarrays(self,arr,n,k):
+        
+        #code here
+        ans=[]
+        res=[]
+        i=0
+        j=0
+        while j<n:
+            if j-i+1<k:
+                heappush(ans,(-arr[j],j))
+            else:
+                heappush(ans,(-arr[j],j))
+                res.append(-ans[0][0])
+                while len(ans) and ans[0][1]<=i:
+                    heappop(ans)
+                i+=1
+            j+=1
+                        
+        return  res
